15f181a3dba6962c27593608a5577fa0ebe068b1
Mangpha <8350130@gmail.com>
2024-03-12 16:08:43 +0900

    Feature: Add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,64 @@
 import {
 	Box,
 	Flex,
-	Grid,
-	GridItem,
+	Heading,
+	InputGroup,
+	InputLeftElement,
+	Input,
+	IconButton,
+	useColorMode,
+	useColorModeValue,
 	HStack,
-	Stack,
-	VStack,
 } from '@chakra-ui/react';
+import { MoonIcon, SearchIcon, SunIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
+import { RiMovie2Line } from 'react-icons/ri';
 
 export default function Header() {
+	const { toggleColorMode } = useColorMode();
+	const Icon = useColorModeValue(MoonIcon, SunIcon);
+
 	return (
-		<Flex justifyContent={'space-between'} bg={'gray.300'} px={20} py={10}>
-			<Grid templateColumns={'1fr 1fr 1fr'} gap={'5rem'}>
-				<Box>asdf</Box>
-				<Box>asdf</Box>
-				<Box>asdf</Box>
-			</Grid>
-			<Box>asdfasdf</Box>
-		</Flex>
+		<Box
+			as={'header'}
+			borderBottom={'1px'}
+			borderColor={'gray.200'}
+			px={20}
+			py={6}
+		>
+			<Flex alignItems={'center'} justifyContent={'space-between'}>
+				<Link to={'/'}>
+					<HStack>
+						<RiMovie2Line size={'2rem'} />
+						<Heading as={'h1'} size={'lg'}>
+							Moble Comics
+						</Heading>
+					</HStack>
+				</Link>
+				<Flex alignItems={'center'}>
+					<InputGroup mr={6}>
+						<InputLeftElement
+							pointerEvents={'none'}
+							children={<SearchIcon />}
+						/>
+						<Input type={'text'} placeholder={'Search movies'} />
+					</InputGroup>
+					<HStack mx={10}>
+						<Box mr={5}>
+							<Link to={'/'}>Home</Link>
+						</Box>
+						<Box>
+							<Link to={'/about'}>About</Link>
+						</Box>
+					</HStack>
+					<IconButton
+						onClick={toggleColorMode}
+						variant={'ghost'}
+						aria-label={'Toggle color mode'}
+						icon={<Icon />}
+					/>
+				</Flex>
+			</Flex>
+		</Box>
 	);
 }
--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,17 @@ import { ThemeConfig } from '@chakra-ui/theme';
 const config: ThemeConfig = {
 	initialColorMode: 'system',
 	useSystemColorMode: true,
+	disableTransitionOnChange: false,
 };
 
-export const theme = extendTheme({ config });
+export const theme = extendTheme({
+	config,
+	styles: {
+		global: {
+			body: {
+				transitionProperty: 'all',
+				transitionDuration: 'normal',
+			},
+		},
+	},
+});
